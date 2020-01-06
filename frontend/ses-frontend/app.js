354d1b580d8bda51ab420087e40f801e5990be2e
Chandan Kumar <chandanust@gmail.com>
2020-01-06 18:23:12 +0530

    updated url

--- frontend/ses-frontend/app.js
+++ frontend/ses-frontend/app.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:5500/api/programs/')
+fetch('http://localhost:3000/api/programs/')
                 .then(response => response.json())
                 .then(programs => {
                     console.log(programs);
